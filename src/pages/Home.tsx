0dd9c6b4c920b4459e9608f31042e44217eb3827
Valmar Munhoz <valmar_mmm@hotmail.com>
2023-02-06 16:56:17 -0300

    refactor: About button removed (feat in development)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,53 +1,50 @@
-import { useNavigate } from "react-router-dom";
-import Characters from '../components/CharacterScreen';
+import { useNavigate } from 'react-router-dom'
+import Characters from '../components/CharacterScreen'
 
-import { AContainer, DivApp, DivContainer, DivFlex } from "../styles/styles";
-import { FiExternalLink } from "react-icons/fi";
+import { AContainer, DivApp, DivContainer, DivFlex } from '../styles/styles'
+import { FiExternalLink } from 'react-icons/fi'
 
 export function Home() {
-    const navigate = useNavigate();
+ const navigate = useNavigate()
 
-    function navigateToNewRoom() {
-        navigate('/About');
-    }
+ function navigateToNewRoom() {
+  navigate('/About')
+ }
 
-    return (
-        <DivApp>
-            <DivContainer>
-                <button>
-                    Rick and Morty!
-                </button>
+ return (
+  <DivApp>
+   <DivContainer>
+    {/* <button>Rick and Morty!</button> */}
 
-                <button
+    {/* <button
                     // onClick={navigateToNewRoom}
                     disabled={true}
                 >
                     About Me!
-                </button>
+                </button> */}
 
-                <AContainer>
-                    <a
-                        href="https://rickandmortyapi.com/api/character"
-                        target="_blank"
-                        rel="noreferrer noopener"
-                    >
-                        <DivFlex>
-                            <h1> Rick and Morty API</h1>
-                            <FiExternalLink
-                                style={{
-                                    position: 'relative',
-                                    left: '15px',
-                                }}
-                                size="60px"
-                                color="white"
-                            />
-                        </DivFlex>
-                    </a>
-                </AContainer>
+    <AContainer>
+     <a
+      href="https://rickandmortyapi.com/api/character"
+      target="_blank"
+      rel="noreferrer noopener"
+     >
+      <DivFlex>
+       <h1> Rick and Morty API</h1>
+       <FiExternalLink
+        style={{
+         position: 'relative',
+         left: '15px'
+        }}
+        size="60px"
+        color="white"
+       />
+      </DivFlex>
+     </a>
+    </AContainer>
 
-                <Characters />
-
-            </DivContainer>
-        </DivApp>
-    )
+    <Characters />
+   </DivContainer>
+  </DivApp>
+ )
 }
